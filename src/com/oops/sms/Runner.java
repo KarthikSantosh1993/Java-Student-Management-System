2a4d1347d4e6bea7e8a93c29205b0fe5b951a7b1
Karthik <krthiksantosh@gmail.com>
2025-12-03 23:30:58 +0530

    Instance variables & Encapsulation

--- src/com/oops/sms/Runner.java
+++ src/com/oops/sms/Runner.java
@@ -3,8 +3,25 @@ package com.oops.sms;
 public class Runner {
 
 	public static void main(String[] args) {
-	
+		Student s1 = new Student();
+		s1.setName("karthik");
+		s1.setAge(25);
+		s1.setRollNumber(0531);
+		s1.setMarksObtainedInEnglish(84);
+		s1.setMarksObtainedInMaths(99);
+		s1.setMarksObtainedInScience(79);
+		s1.setGrade("A");
+		
+		System.out.println(s1.getName());
 
+		System.out.println(s1.getAge());
+		System.out.println(s1.getRollNumber());
+		System.out.println(s1.getMarksObtainedInEnglish());
+		System.out.println(s1.getMarksObtainedInMaths());
+		System.out.println(s1.getMarksObtainedInScience());
+		System.out.println(s1.getGrade());
+		
+		s1.calculateToalmarks();
 	}
 
 }
--- src/com/oops/sms/Student.java
+++ src/com/oops/sms/Student.java
@@ -1,6 +1,98 @@
 package com.oops.sms;
 
 public class Student {
+	private String name;
+	private int age;
+	private int rollNumber;
+	private int marksObtainedInEnglish;
+	private int marksObtainedInMaths;
+	private int marksObtainedInScience;
+	private String grade;
 
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age >= 10 && age < 21) {
+			this.age = age;
+		} else {
+			System.out.println("Invalid age for Student ");
+		}
+
+	}
+
+	public int getRollNumber() {
+		return rollNumber;
+	}
+
+	public void setRollNumber(int rollNumber) {
+		if (rollNumber >= 1) {
+			this.rollNumber = rollNumber;
+		} else {
+			System.out.println("invalid Roll number: ");
+		}
+
+	}
+
+	public int getMarksObtainedInEnglish() {
+		return marksObtainedInEnglish;
+
+	}
+
+	public void setMarksObtainedInEnglish(int marksObtainedInEnglish) {
+
+		if (marksObtainedInEnglish >= 0 && marksObtainedInEnglish <= 100) {
+			this.marksObtainedInEnglish = marksObtainedInEnglish;
+		} else {
+			System.out.println("invalid marks for English ");
+		}
+
+	}
+
+	public int getMarksObtainedInMaths() {
+		return marksObtainedInMaths;
+	}
+
+	public void setMarksObtainedInMaths(int marksObtainedInMaths) {
+		if (marksObtainedInMaths >= 0 && marksObtainedInMaths <= 100) {
+			this.marksObtainedInMaths = marksObtainedInMaths;
+		} else {
+			System.out.println("invalid marks for Maths ");
+		}
+	}
+
+	public int getMarksObtainedInScience() {
+		return marksObtainedInScience;
+	}
+
+	public void setMarksObtainedInScience(int marksObtainedInScience) {
+		if (marksObtainedInScience >= 0 && marksObtainedInScience <= 100) {
+			this.marksObtainedInScience = marksObtainedInScience;
+		} else {
+			System.out.println("invalid marks for Science ");
+		}
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public void calculateToalmarks() {
+		double totalmarks = marksObtainedInEnglish + marksObtainedInMaths + marksObtainedInScience;
+		System.out.println("Total Marks obtained= " + totalmarks);
+	}
+
 }
